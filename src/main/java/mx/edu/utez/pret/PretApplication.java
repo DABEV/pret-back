e8ce80cf614634c100a3f8885bcf0c86a5b97dac
GandyA23 <avila.gandy@gmail.com>
2022-06-12 13:13:25 -0500

    Init Project with Spring Framework
    Init Project with Spring Framework, it is the next characteristics:
     - Spring Boot v2.7.0
     - Maven
     - Java v11
     - Packaging: Jar
    
    Init Dependencies:
     - Spring Boot DevTools
     - Spring Web

---
+++ src/main/java/mx/edu/utez/pret/PretApplication.java
@@ -0,0 +1,13 @@
+package mx.edu.utez.pret;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PretApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PretApplication.class, args);
+	}
+
+}
